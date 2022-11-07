be23e959e4bb4bf23b67f15f7a88f6033cb3e476
wodyddml2 <wodyddml4@naver.com>
2022-11-07 15:05:05 +0900

    Initial Commit

---
+++ SeSAC_StudyMatcingApp/Presentation/OnboardingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_StudyMatcingApp
+//
+//  Created by J on 2022/11/07.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
