d0172ab6725b836dc6c8fb316d96f890203b19b5
wodyddml2 <wodyddml4@naver.com>
2022-12-13 14:22:12 +0900

    Fix: 상대방이 스터디 수락 후 채팅 내역, 상대방 닉네임이 뜨지 않는 버그 개선
    (#143)

--- SeSAC_StudyMatcingApp/Presentation/SeSACScene/HomeScene/SeSACFindScene/TabMan/TabManSeSACViewController.swift
+++ SeSAC_StudyMatcingApp/Presentation/SeSACScene/HomeScene/SeSACFindScene/TabMan/TabManSeSACViewController.swift
@@ -119,14 +119,13 @@ final class TabManSeSACViewController: TabmanViewController {
         viewModel.match
             .withUnretained(self)
             .bind (onNext: { vc, result in
-                let viewController = ChattingViewController()
-
                 if result.matched == 1 {
                     vc.view.makeToast("\(result.matchedNick ?? "")님과 매칭되셨습니다. 잠시 후 채팅방으로 이동합니다.", duration: 1) { _ in
                         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            let viewController = ChattingViewController()
                             viewController.viewModel.nickname = result.matchedNick ?? ""
                             viewController.viewModel.uid = result.matchedUid ?? ""
-                            vc.transition(ChattingViewController(), transitionStyle: .push)
+                            vc.transition(viewController, transitionStyle: .push)
                         }
                     }
                 }
